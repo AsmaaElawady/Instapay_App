9d71ac2e339e0fa46c1abd067ce62a75973f550f
__Rawan__ <rawanroni2003@gmail.com>
2023-11-15 13:05:11 +0200

    Revert "Merge branch 'main' of https://github.com/AsmaaElawady/Instapay_App"
    This reverts commit e837f1d9953ce8e20d97947669a4209521303c43, reversing
    changes made to 296fea59998953985d5917a33c5df106d3386d60.

--- out/production/Instapay_App/ElectricityFacility.java
+++ out/production/Instapay_App/ElectricityFacility.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
-public class WaterFacility  implements Facilities{
-
-    
+public class ElectricityFacility implements Facilities{
 
     @Override
     public Bill createBill(Account account) {
-        
+
         Scanner scanner = new Scanner(System.in);
         int choice;
 
-        System.out.println("Types of service:\n"  +  "1- Smart card\n" + "2- Bill\n");
-        
+        System.out.println("Types of service   1-Smart card 2-Bill 3-Smart meter");
+        System.out.print("Enter your choice:");
+        choice = scanner.nextInt();
+
         while (true) {
             System.out.print("Enter your choice:");
             choice = scanner.nextInt();
-            if (choice != 1 || choice != 2) {
+            if (choice != 1 || choice != 2 || choice != 3) {
                 System.out.print("invalid choice:");
                 continue;
             }else {
@@ -26,13 +26,16 @@ public class WaterFacility  implements Facilities{
         Bill bill = null;
         switch (choice) {
             case 1:
-               bill =  new WaterSmartCardBill(account);
+               bill =  new ElectricitySmartCardBill(account);
                break;
         
-             case 2:
-                bill = new WaterTradBill(account);
+            case 2:
+                bill = new ElectricityTradBill(account);
                 break;
-                
+
+            case 3:
+                bill = new ElectricitySmartMeterBill(account);
+                break;
 
             default:
                 break;
@@ -41,9 +44,7 @@ public class WaterFacility  implements Facilities{
 
         return bill;
         
-
-    
-        
     }
+
     
 }
--- out/production/Instapay_App/WaterFacility.java
+++ out/production/Instapay_App/WaterFacility.java
@@ -1,41 +1,37 @@
 import java.util.Scanner;
 
-public class ElectricityFacility implements Facilities{
+public class WaterFacility  implements Facilities{
+
+    
 
     @Override
     public Bill createBill(Account account) {
-
+        
         Scanner scanner = new Scanner(System.in);
         int choice;
 
-        System.out.println("Types of service:\n" + "1-Smart card\n" + "2-Bill\n" + "3-Smart meter\n");
-
-        
-
+        System.out.println("Types of service   1- Smart card 2- Bill");
         while (true) {
             System.out.print("Enter your choice:");
             choice = scanner.nextInt();
-            if (choice == 1 || choice == 2 || choice == 3) {
-                break;
-            }else {
+            if (choice != 1 || choice != 2) {
                 System.out.print("invalid choice:");
                 continue;
+            }else {
+                break;
             }
         }
 
         Bill bill = null;
         switch (choice) {
             case 1:
-               bill =  new ElectricitySmartCardBill(account);
+               bill =  new WaterSmartCardBill(account);
                break;
         
-            case 2:
-                bill = new ElectricityTradBill(account);
+             case 2:
+                bill = new WaterTradBill(account);
                 break;
-
-            case 3:
-                bill = new ElectricitySmartMeterBill(account);
-                break;
+                
 
             default:
                 break;
@@ -44,7 +40,9 @@ public class ElectricityFacility implements Facilities{
 
         return bill;
         
-    }
 
+    
+        
+    }
     
 }
